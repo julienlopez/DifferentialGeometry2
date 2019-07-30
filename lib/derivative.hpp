97acc0a627c9d651f1227eefa50976747e5dc00a
julien lopez <julien.lopez@stilla.fr>
2019-07-30 15:33:11 +0200

    trying to fix build on gcc

--- lib/derivative.hpp
+++ lib/derivative.hpp
@@ -2,6 +2,8 @@
 
 #include "expression.hpp"
 
+#include <stdexcept>
+
 struct Derivative : public boost::static_visitor<Expression>
 {
     template <class T> Expression operator()(const T& t) const
